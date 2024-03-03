7d2e4e06fe5492988e543fc3268a66b51b0c1d8f
cltubigon <cltubigon@gmail.com>
2024-03-03 18:57:38 +0800

    added video section

--- src/app/recipient-sydney/page.js
+++ src/app/recipient-sydney/page.js
@@ -5,12 +5,18 @@ import RecipientProfile from "./RecipientProfile"
 import HugMessageShare from "./HugMessageShare"
 import PackageSection from "./PackageSection"
 import FifthSection from "./FifthSection"
-// import VideoSection from "./VideoSection"
+import VideoSection from "./VideoSection"
 import Testimonials from "./Testimonials"
 import WristHugSection from "./WristHugSection"
 import WhatIsSection from "./WhatIsSection"
 import CommentSection from "./CommentSection"
 import Footer from "./Footer"
+
+export const metadata = {
+  title: "Welcome to {Name}'s Page!",
+  description:
+    "She was riding her bike one day and suddenly felt sick. Her parents took her to the hospital where she was diagnosed with DIPG (a difficult to treat brain tumor). The doctors began taking care of {Name} right away and she is back home resting. She likes Unicorns and dancing and she hopes to meet Beyonce one day.",
+}
 
 const RecipientsPage = () => {
   return (
@@ -21,7 +27,7 @@ const RecipientsPage = () => {
       <HugMessageShare />
       <PackageSection />
       <FifthSection />
-      {/* <VideoSection /> */}
+      <VideoSection />
       <Testimonials />
       <WristHugSection />
       <WhatIsSection />
