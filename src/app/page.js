ccfc7ad954efb9facef6a5c1f0718f58608bd4e4
cltubigon <cltubigon@gmail.com>
2024-03-06 04:21:45 +0800

    implemented generateStaticParams

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
 import { supabase } from "@/config/supabase"
-import { Button, Flex, Heading, Text } from "@chakra-ui/react"
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react"
 import { containerInner, containerPadding } from "./globalStyle"
 import Link from "next/link"
-import Recipients from "./Recipients"
 
-const HomePage = () => {
+export const revalidate = 20
+
+const getRecipient = async () => {
+  let { data: recipients, error } = await supabase
+    .from("recipients")
+    .select("*")
+  if (recipients) {
+    return recipients
+  }
+  if (error) console.log({ error })
+}
+
+const HomePage = async () => {
+  const recipients = await getRecipient()
   return (
     <Flex
       sx={containerPadding}
@@ -18,7 +30,23 @@ const HomePage = () => {
         alignItems={"flex-start"}
         gap={4}
       >
-        <Recipients />
+        <Flex flexDir={"column"} justifyContent={"center"}>
+          <Heading>List of Recipients </Heading>
+          <Text>{"(Click a recipient below to view) v0.0.2"}</Text>
+        </Flex>
+
+        {/* <Box>{JSON.stringify(recipients, null, 2)}</Box> */}
+
+        {recipients?.map((recipient, index) => {
+          const { first_name } = recipient
+          return (
+            <div key={index}>
+              <Link href={`/recipient/${first_name}`} shallow>
+                <Text fontSize={"32px"}>{first_name}</Text>
+              </Link>
+            </div>
+          )
+        })}
       </Flex>
       <Flex h={"100vh"} w={"100%"}></Flex>
       <Flex h={"100vh"} w={"100%"}></Flex>
--- src/app/recipient/[first_name]/page.js
+++ src/app/recipient/[first_name]/page.js
@@ -13,6 +13,7 @@ import CommentSection from "./CommentSection"
 import Footer from "./Footer"
 import { supabase } from "@/config/supabase"
 import { notFound } from "next/navigation"
+import { Box } from "@chakra-ui/react"
 
 export const metadata = {
   title: "Welcome to {Name}'s Page!",
@@ -20,31 +21,33 @@ export const metadata = {
     "She was riding her bike one day and suddenly felt sick. Her parents took her to the hospital where she was diagnosed with DIPG (a difficult to treat brain tumor). The doctors began taking care of {Name} right away and she is back home resting. She likes Unicorns and dancing and she hopes to meet Beyonce one day.",
 }
 
-const getRecipient = async (name) => {
-  let { data: recipients, error } = await supabase
-    .from("recipients")
-    .select("*")
-    .ilike("first_name", name)
-  if (recipients) {
-    return recipients[0]
-  }
-  if (error) console.log({ error })
+export const revalidate = 10
+
+export const generateStaticParams = async () => {
+// export async function generateStaticParams() {
+  const { data: recipients } = supabase.from("recipients").select("first_name")
+  return recipients || []
 }
 
-const RecipientsPage = async ({ params }) => {
-  const { first_name } = params
-  const recipient = await getRecipient(first_name)
-  if (!recipient) {
+const RecipientsPage = async ({ params: { first_name } }) => {
+  // const name = capitalize(first_name)
+  // console.log({ name })
+  const { data } = await supabase
+    .from("recipients")
+    .select()
+    .ilike('first_name', first_name)
+  if (!data) {
     notFound()
+  } else {
+    console.log("data", data)
   }
-  const {first_name: firstName, hugs, created_at, category} = recipient
-  // console.log("recipient", recipient)
+  const recipient = data[0]
+  const { first_name: firstName, hugs, created_at, category } = recipient
   return (
     <>
+      {/* <Box>{JSON.stringify(recipient)}</Box> */}
       <LogoSection />
-      <TitleSection
-      parameters={{ firstName, category, created_at }}
-      />
+      <TitleSection parameters={{ firstName, category, created_at }} />
       <RecipientProfile recipient={recipient} />
       <HugMessageShare parameters={{ firstName, hugs }} />
       <PackageSection />
